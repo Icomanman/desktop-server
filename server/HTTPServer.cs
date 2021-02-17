b4ec7edc81699066e9ba94c11ebfaf01d86f76c4
icomanman <fred_barabs@yahoo.com>
2021-02-18 00:31:47 +0800

    adding initial files for setup.

---
+++ server/HTTPServer.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace server
+{
+	public class HTTPServer
+	{
+
+		private bool isRunning = false;
+
+		private TcpListener listener;
+
+		public HTTPServer(int port)
+		{
+			listener = new TcpListener(IPAddress.Any, port);
+		}
+
+		public void Start()
+		{
+			Thread serverThread = new Thread(new ThreadStart(Run));
+		}
+
+		private void Run()
+		{
+			isRunning = true;
+
+			while (isRunning)
+			{
+				TcpClient client = listener.AcceptTcpClient();
+
+				HandleClient(client);
+				client.Close();
+			}
+
+			isRunning = false;
+			listener.Stop();
+		}
+
+		private void HandleClient(TcpClient client)
+		{
+
+		}
+	}
+}
---
+++ server/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace server
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+    }
+  }
+}
