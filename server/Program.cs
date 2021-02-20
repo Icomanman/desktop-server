590161cb933818de22b4d64af71de8c3c7e774c2
icomanman <fred_barabs@yahoo.com>
2021-02-20 12:24:14 +0800

    added a Simple-HTTP server within Program.cs

--- server/Program.cs
+++ server/Program.cs
@@ -1,11 +1,23 @@
 ﻿using System;
-
+using SimpleHttp;
+using System.Threading;
 namespace server
 {
   class Program
   {
     static void Main(string[] args)
     {
+			Route.Add("/", (req, res, props) =>
+			{
+				res.AsText("Pasok!");
+			});
+
+
+			HttpServer.ListenAsync(
+				1630, 
+				CancellationToken.None,
+				Route.OnHttpRequestAsync
+				).Wait();
     }
   }
 }
