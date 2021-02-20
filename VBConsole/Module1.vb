7b2d9e110873eda2974e22ab58cf7c3c970cd89e
icomanman <fred_barabs@yahoo.com>
2021-02-20 22:20:37 +0800

    added VB for testing Staad.

---
+++ VBConsole/Module1.vb
@@ -0,0 +1,20 @@
+﻿Imports OpenSTAADUI
+Module Module1
+
+    Sub Main()
+        Dim OSt As Object
+        Dim path As String
+
+        path = "C:\Users\GiulianoDiEnrico\Documents\010 Swan House\Outgoing\B Calcs\STAAD Models\SH-011.std"
+        'OSt = CreateObject("StaadPro.OpenSTAAD")
+        OSt = GetObject(path, "StaadPro.OpenSTAAD")
+        Console.ReadKey()
+
+        OSt.Quit()
+
+        Console.WriteLine("tawid")
+
+        OSt = Nothing
+    End Sub
+
+End Module
