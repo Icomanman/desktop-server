e22dc456adc641d2b610c594cd034f76e1ea06a7
icomanman <fred_barabs@yahoo.com>
2021-03-07 01:44:16 +0800

    updated server for POST request

--- server/Program.cs
+++ server/Program.cs
@@ -1,10 +1,7 @@
-﻿using server.Staad.src;
-using server.Robot.src;
+﻿
 using SimpleHttp;
 using System;
 using System.Threading;
-using OpenSTAADUI;
-using VBConsole;
 
 namespace server
 {
@@ -12,48 +9,22 @@ namespace server
     {
         public static void Main(string[] args)
         {
-            OpenSTAADClass OSt = null;
-            string path = @"C:\Users\GiulianoDiEnrico\Documents\010 Swan House\Outgoing\B Calcs\STAAD Models\SH-011.std";
 
-            Route.Add("/", (req, res, props) =>
+            Route.Add("/api", (req, res, props) =>
             {
                 res.AsText("pasok!");
-                Console.WriteLine("Opening a Staad Instance...");
-                // Module1.Main();
-                OSt = Module1.GetStaad(path, true) as OpenSTAADClass;
-                Console.WriteLine(OSt);
-            });
-
-            Route.Add("/close", (req, res, props) =>
-            {
-                res.AsText("closing...");
-                Console.WriteLine("Closing Staad Instance...");
-                if(OSt != null)
-                {
-                    OSt.Quit();
-                }
-                else
-                {
-                    Console.WriteLine("null");
-                    Module1.GetStaad(path, false);
-                }
                 
-            });
+                res.StatusCode = 200;
+                Console.WriteLine("Opening a Staad Instance...");
+                
+                if (req != null)
+                {
+                    Console.WriteLine(req.Headers.ToString());
+                }
+                res.Close();
+            }, "POST");
 
-            HttpServer.ListenAsync(
-                1630,
-                CancellationToken.None,
-                Route.OnHttpRequestAsync
-                ).Wait();
-
-            // OSt = StaadMain.Connect();
-
-            if (OSt == null)
-            {
-                Console.WriteLine("No instance of Staad was found.");
-            }
-
-            // StaadMain.OpenApp();
+            HttpServer.ListenAsync(1630, CancellationToken.None, Route.OnHttpRequestAsync).Wait();
 
             Console.ReadKey();
 
