fba3b4b469133a0d129de1776b82657aed118d2a
icomanman <fred_barabs@yahoo.com>
2021-03-07 23:00:57 +0800

    server updates

--- server/Program.cs
+++ server/Program.cs
@@ -2,6 +2,8 @@
 using SimpleHttp;
 using System;
 using System.Threading;
+using System.IO;
+using System.Text;
 
 namespace server
 {
@@ -12,17 +14,32 @@ namespace server
 
             Route.Add("/api", (req, res, props) =>
             {
-                res.AsText("pasok!");
-                
-                res.StatusCode = 200;
-                Console.WriteLine("Opening a Staad Instance...");
-                
                 if (req != null)
                 {
-                    Console.WriteLine(req.Headers.ToString());
+                    Stream body = req.InputStream;
+                    Encoding encoding = req.ContentEncoding;
+                    StreamReader reader = new StreamReader(body, encoding);
+
+                    Console.WriteLine("Content Type: {0}", req.ContentType);
+
+                    string s = reader.ReadToEnd();
+                    Console.WriteLine("Path: {0}", s);
+
+                    body.Close();
+                    reader.Close();
                 }
+
+                res.AsText("pasok!");
+                res.StatusCode = 200;
                 res.Close();
             }, "POST");
+
+            Route.Add("/api", (req, res, props) =>
+            {
+                res.AsText("pasok!");
+                res.StatusCode = 200;
+                res.Close();
+            });
 
             HttpServer.ListenAsync(1630, CancellationToken.None, Route.OnHttpRequestAsync).Wait();
 
